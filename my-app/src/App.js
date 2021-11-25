41c4fd6c811ec2464bbcd3aa83e6c88506b5791f
Menim Joseph <59745948+mensco43@users.noreply.github.com>
2021-11-25 02:13:32 +0100

    intial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,58 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div className="container">
+      <h1>Spacious</h1>
+      <div className="text">
+        <h5>Plannet</h5>
+        <p>Characters</p>
+      </div>
+      <div className= "files">
+      <div className="first-file">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="text-2">
+        <h6>Planent Alpha</h6>
+        <p>pop:235</p>
+        </div>
+      </div>
+      <div className="first-file">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="text-2">
+        <h6>Planent Alpha</h6>
+        <p>pop:235</p>
+        </div>
+      </div>
+      <div className="first-file">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="text-2">
+        <h6>Planent Alpha</h6>
+        <p>pop:235</p>
+        </div>
+      </div>
+      </div>
+    </div>
+  </div> 
+  );
+}
+
+export default App;
+
+
+
+<header className="App-header">
+
+<p>
+  Edit <code>src/App.js</code> and save to reload.
+</p>
+<a
+  className="App-link"
+  href="https://reactjs.org"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  Learn React
+</a>
+</header>
